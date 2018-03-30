0890046a93468a02d1b35c113e1ba94a3efd73e3
Your Name <you@example.com>
2018-03-30 13:18:13 +0300

    Step #5

--- Queue_Project/main.c
+++ Queue_Project/main.c
@@ -8,52 +8,47 @@
 typedef char type;
 typedef struct Queue {
   type *array;
-  int deqloc, enqloc;
+  int deqloc, enqloc, now_size, max_size;
 } queue;
 
-void initQueue(queue **q) {
+void initQueue(queue **q, int size) {
   *q = malloc(sizeof(queue));
-  (*q)->array = calloc(QUEUE_SIZE + 1, sizeof(type));
+  (*q)->max_size = size;
+  (*q)->array = calloc((*q)->max_size, sizeof(type));
   (*q)->deqloc = 0;
   (*q)->enqloc = 0;
+  (*q)->now_size = 0;
 }
 
-int enq(queue *q, type elem) {
-  if ((q->enqloc + 1 == q->deqloc) ||
-      (q->enqloc == QUEUE_SIZE && q->deqloc == 0))
-    return 0;
-  q->enqloc = (q->enqloc + 1) % (QUEUE_SIZE + 1);
-  q->array[q->enqloc] = elem;
-  return 1;
-}
-
-type deq(queue *q) {
-  if (q->enqloc == q->deqloc)
+type deq(queue **q) {
+  if ((*q)->now_size == 0)
     return ERROR_DEQ;
-  q->deqloc = (q->deqloc + 1) % (QUEUE_SIZE + 1);
-  return q->array[q->deqloc];
+  type elem = (*q)->array[(*q)->deqloc];
+  (*q)->deqloc = ((*q)->deqloc + 1) % ((*q)->max_size);
+  (*q)->now_size--;
+  return elem;
+}
+
+void enq(queue **q, type elem) {
+  if ((*q)->now_size == (*q)->max_size) 
+    deq(q);
+  (*q)->array[(*q)->enqloc] = elem;
+  (*q)->enqloc = ((*q)->enqloc + 1) % ((*q)->max_size);
+  (*q)->now_size++;
 }
 
 type top(queue *q) {
-  if (q->enqloc == q->deqloc)
+  if (q->now_size == 0)
     return ERROR_DEQ;
-  int index = (q->deqloc + 1) % (QUEUE_SIZE + 1);
-  return q->array[index];
+  return q->array[q->deqloc];
 }
 
 int isEmpty(queue *q) {
-  if (q->deqloc == q->enqloc)
-    return 1;
-  return 0;
+  return !q->now_size;
 }
 
 int count(queue *q) {
-  int result = 0;
-  for (int i = q->enqloc; i != q->deqloc; i--) {
-    if (i == -1) i = QUEUE_SIZE;
-    result++;
-  }
-  return result;
+  return q->now_size;
 }
 
 void finalize(queue *q) {
@@ -73,29 +68,26 @@ void intro(queue **q, char **string) {
   printf("Data Tester is the programm which is used to test various data structures.\n");
   printf("This version is specialized on the cyclical array-based queue, which size is %d.\n\n", QUEUE_SIZE);
 
-  initQueue(q);
+  initQueue(q, QUEUE_SIZE);
   *string = malloc(QUEUE_SIZE * sizeof(char)); 
 }
 
-void instruction_1(queue *q, char *string) {
+void instruction_1(queue **q, char *string) {
   int count = 0;
 
-  printf("Write a string and press Enter.\nFirst %d symbols (except '\\n') will be tried to push into the queue.\n", QUEUE_SIZE);
-  fgets(string, QUEUE_SIZE + 1, stdin);
-  if (strlen(string) >= QUEUE_SIZE)
-    while(getchar() != '\n');
+  printf("Write a string and press Enter.\n");
+  fgets(string, 100, stdin);
   if (strstr(string, "\n"))
     string[strlen(string) - 1] = '\0';
   for (int i = 0; string[i]; i++) {
-    if (!enq(q, string[i]))
-      break;
+    enq(q, string[i]);
     count++;
   }
   printf("You added %d elements.\n", count);
-  showInfo(q);
+  showInfo(*q);
 }
 
-void instruction_2(queue *q) {
+void instruction_2(queue **q) {
   int number, count = 0;
 
   printf("Write the number of elements you want do delete: ");
@@ -106,7 +98,7 @@ void instruction_2(queue *q) {
     count++;
   }
   printf("You deleted %d elements.\n", count);
-  showInfo(q);
+  showInfo(*q);
 }
 
 int main() {
@@ -114,8 +106,8 @@ int main() {
   char *string;
 
   intro(&qu, &string);
-  instruction_1(qu, string);
-  instruction_2(qu);
+  instruction_1(&qu, string);
+  instruction_2(&qu);
   printf("Bye-bye!\n");
   finalize(qu);
   free(string);
