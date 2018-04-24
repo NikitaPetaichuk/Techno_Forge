3a8df88aeafc02a38139b184828315ca8f4f6ab9
NikitaPetaichuk <31537936+NikitaPetaichuk@users.noreply.github.com>
2018-04-24 18:02:43 +0300

    LR_3

--- Nikita_Petaichuk_lr3/solution.c
+++ Nikita_Petaichuk_lr3/solution.c
@@ -5,10 +5,7 @@
 #include <dirent.h>
 #include <regex.h>
 
-#define FILE_NUMBER 5000
-#define STRING_SIZE 150
-#define NUMBER_SIZE 64
-#define F_NUMBER_STEP 1000
+#define STRING_SIZE 200
 #define RESULT_FILE "result.txt"
 #define START_DIR "root"
 
@@ -38,20 +35,16 @@ int compFileData(const void *a, const void *b) {
 }
 
 void readData(char *filename, filedata **array, int *pcount, int *pmax) {
-  char digit[NUMBER_SIZE] = {0};
+  char digit[STRING_SIZE] = {0};
   FILE *file = fopen(filename, "r");
   int i;
 
   if (!file) return;
-  if (*pcount == FILE_NUMBER) {
-    *pmax += F_NUMBER_STEP;
-    *array = realloc(*array, sizeof(filedata) * (*pmax));
-  }
-  fgets(array[*pcount]->string, STRING_SIZE, file);
-  for (i = 0; array[*pcount]->string[i] != ' '; i++)
-    digit[i] = array[*pcount]->string[i];
-  array[*pcount]->number = atoll(digit);
-//  printf("\t\t%lld - %s", array[*pcount]->number, array[*pcount]->string);
+  *array = realloc(*array, sizeof(filedata) * ((*pcount) + 1));
+  fgets((*array)[*pcount].string, STRING_SIZE, file);
+  for (i = 0; (*array)[*pcount].string[i] != ' '; i++)
+    digit[i] = (*array)[*pcount].string[i];
+  (*array)[*pcount].number = atoll(digit);
   (*pcount)++;
   fclose(file);
   return;
@@ -67,7 +60,6 @@ void goThroughDirs(char *root, filedata **array, int *pcount, int *pmax) {
   while(now) {
     if(now->d_type == DT_DIR && strcmp(now->d_name, ".") != 0 &&
        strcmp(now->d_name, "..") != 0) {
-//      printf("%s:\n", now->d_name);
       int len = strlen(next);
       strcat(next, "/");
       strcat(next, now->d_name);
@@ -75,7 +67,6 @@ void goThroughDirs(char *root, filedata **array, int *pcount, int *pmax) {
       next[len] = '\0';
     }
     if(now->d_type == DT_REG && isCorrectFile(now->d_name)) {
-//      printf("\t%s:\n", now->d_name);
       int len = strlen(next);
       strcat(next, "/");
       strcat(next, now->d_name);
@@ -84,20 +75,20 @@ void goThroughDirs(char *root, filedata **array, int *pcount, int *pmax) {
     }
     now = readdir(dir);
   }
-  free(next);
   closedir(dir);
+  free(next);
   return;
 }
 
 int main() {
-  filedata *array = calloc(FILE_NUMBER, sizeof(filedata));
-  int count = 0, max = FILE_NUMBER, i;
+  filedata *array = (filedata *)malloc(sizeof(filedata));
+  int count = 0, i;
   FILE *output;
 
-  goThroughDirs(START_DIR, &array, &count, &max);
+  goThroughDirs(START_DIR, &array, &count);
   qsort(array, count, sizeof(filedata), compFileData);
   output = fopen(RESULT_FILE, "w");
-  for (i = 0; i < count; i++) 
+  for (i = 0; i < count; i++)
     fwrite(array[i].string, sizeof(char), strlen(array[i].string), output);
   fclose(output);
   free(array);
